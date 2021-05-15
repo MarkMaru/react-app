381e6bbdb4809587573669b8523c0f130118bf07
MarkMaru <78791564+MarkMaru@users.noreply.github.com>
2021-05-15 09:30:36 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ReactDOM from 'react-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Coming soon!
-        </p>
-      </header>
-    </div>
+
+const App = () => {
+  const [hex, setHex] = useState("#ffffff");
+  const randomizedHex = () => {
+  
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    setHex(randomColor);
+    
+    var self = {};
+    const d1 = document.createElement("li");
+    self.d1 = document.getElementById('list').innerHTML = `${hex}`;   
+     
+  }
+
+  
+
+  return(
+  <div className = "App">
+    <button onClick={randomizedHex}><h2 style={{color: `${hex}`,}}>Click me</h2></button>
+    <ol id="list">
+    </ol>
+  </div>
   );
 }
 
