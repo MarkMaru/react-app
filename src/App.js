67d7ff3f233a148b40b0f5f0050f75976b0237ac
MarkMaru <78791564+MarkMaru@users.noreply.github.com>
2021-05-16 23:16:05 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
-import ReactDOM from 'react-dom'
 import './App.css';
 
+function App () {
 
-const App = () => {
   const [hex, setHex] = useState("#ffffff");
+
   const randomizedHex = () => {
   
-    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
-    setHex(randomColor);
-    
-    var self = {};
-    const d1 = document.createElement("li");
-    self.d1 = document.getElementById('list').innerHTML = `${hex}`;   
-     
+  setHex(randomColor);
+
+  if(document.getElementById("test").innerHTML === hex){
+    alert('Color updated!');
   }
 
-  
+  else if (document.getElementById("test").innerHTML !== hex){
+    alert('Not a hex value');
+  }
+    
+  var self = {};
+  var d1 = document.createElement("li");
+  var textnode = document.createTextNode(`${hex}`);
+  d1.appendChild(textnode);
+  self.d1 = document.getElementById('list').appendChild(d1);   
+     
+  }
 
   return(
   <div className = "App">
     <button onClick={randomizedHex}><h2 style={{color: `${hex}`,}}>Click me</h2></button>
-    <ol id="list">
-    </ol>
+    <input id="test" className="input" placeholder="Type the color in hex here" onChange={event => setHex(event.target.value)}></input>
+    <h1 id="list"></h1>
   </div>
   );
 }
